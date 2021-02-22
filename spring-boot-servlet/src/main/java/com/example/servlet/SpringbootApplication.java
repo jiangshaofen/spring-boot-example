1ec3bdeb1b6ccb59a9e30b744e77edc2e1f420e4
jiang <jiangshaofen@hotmail.com>
2021-02-22 21:59:05 +0800

    add spring boot servlet

---
+++ spring-boot-servlet/src/main/java/com/example/servlet/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan(basePackages = { "com.example.servlet" })
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
